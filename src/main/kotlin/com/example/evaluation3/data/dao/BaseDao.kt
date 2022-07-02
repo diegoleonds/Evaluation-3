37f1f6062bc2ea46217c3987c5ca318bfe8f5f8c
Diego Santos <diegosantos@DiegoLeon-WP9YYHF0GV.local>
2022-07-01 22:32:57 -0300

    basic dao functions implemented (missing update method)

--- src/main/kotlin/com/example/evaluation3/data/dao/BaseDao.kt
+++ src/main/kotlin/com/example/evaluation3/data/dao/BaseDao.kt
@@ -1,13 +1,17 @@
 package com.example.evaluation3.data.dao
 
 import com.example.evaluation3.data.model.BaseEntity
-import jakarta.persistence.Persistence
+import jakarta.persistence.*
 import java.util.*
 
 abstract class BaseDao<E : BaseEntity> : Dao<E> {
 
-    protected val manager by lazy {
-        Persistence.createEntityManagerFactory("meuPU").createEntityManager()
+    protected val manager: EntityManager by lazy {
+        Persistence.createEntityManagerFactory("myDb").createEntityManager()
+    }
+
+    protected val transaction: EntityTransaction by lazy {
+        manager.transaction
     }
 
     abstract fun getEntityClass(): Class<E>
@@ -21,23 +25,39 @@ abstract class BaseDao<E : BaseEntity> : Dao<E> {
         }
     }
 
+    override fun insert(entity: E) {
+        createTransaction {
+            manager.persist(entity)
+        }
+    }
+
     override fun getBydId(id: Long): E? {
-        TODO("Not yet implemented")
+        return manager.createQueryForEntityClass("FROM ${getEntityTableName()} where id = :id").run {
+            setParameter("id", id)
+            singleResult
+        }
     }
 
     override fun getEntities(offset: Int, page: Int): List<E> {
-        TODO("Not yet implemented")
-    }
-
-    override fun insert(entity: E) {
-        TODO("Not yet implemented")
-    }
-
-    override fun delete(entity: E) {
-        TODO("Not yet implemented")
+        return manager.createQueryForEntityClass("FROM ${getEntityTableName()}").resultList ?: emptyList()
     }
 
     override fun update(entity: E) {
-        TODO("Not yet implemented")
+
     }
+
+    override fun delete(entity: E) {
+        createTransaction {
+            manager.remove(entity)
+        }
+    }
+
+    protected fun createTransaction(action: () -> Unit) {
+        transaction.begin()
+        action()
+        transaction.commit()
+    }
+
+    // doc ref https://kotlinlang.org/docs/extensions.html#extension-functions
+    protected fun EntityManager.createQueryForEntityClass(query: String): TypedQuery<E> = createQuery(query, getEntityClass())
 }
--- src/main/kotlin/com/example/evaluation3/data/dao/Dao.kt
+++ src/main/kotlin/com/example/evaluation3/data/dao/Dao.kt
@@ -3,9 +3,9 @@ package com.example.evaluation3.data.dao
 import com.example.evaluation3.data.model.BaseEntity
 
 interface Dao<E : BaseEntity> {
+    fun insert(entity: E)
     fun getBydId(id: Long): E?
     fun getEntities(offset: Int, page: Int): List<E>
-    fun insert(entity: E)
-    fun delete(entity: E)
     fun update(entity: E)
+    fun delete(entity: E)
 }
