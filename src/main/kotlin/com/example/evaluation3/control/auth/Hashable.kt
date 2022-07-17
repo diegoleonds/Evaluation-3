259d1d72193dd16cec3b2ea94ff9ff8598fcbd41
Diego Santos <diegosantos@DiegoLeon-WP9YYHF0GV.local>
2022-07-17 11:38:31 -0300

    hash functions implemented

---
+++ src/main/kotlin/com/example/evaluation3/control/auth/Hashable.kt
@@ -0,0 +1,12 @@
+package com.example.evaluation3.control.auth
+
+import java.security.MessageDigest
+
+private fun hashString(input: String, algorithm: String): String {
+    return MessageDigest
+        .getInstance(algorithm)
+        .digest(input.toByteArray())
+        .fold("") { str, it -> str + "%02x".format(it) }
+}
+
+fun String.sha256() = hashString(this, "SHA-256")
